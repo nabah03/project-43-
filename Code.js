dc14c9c64c0aa4b9ad86efc44ef4bb50d0f33cb7
nabah03 <73787185+nabah03@users.noreply.github.com>
2021-05-20 21:15:52 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+    textSize(15)
+    text("A T A D A S B E", 680,160)
+    fill("lightblue")
+    text("Hint: Our variables are stored here!!", 680,180)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,350)
+    fill("lightblue")
+    text("Hint: A way of telling the computer to run that set of actions ", 100,370)   
+
+
+}
